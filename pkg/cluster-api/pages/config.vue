a7b0743b98ca9b41e2a24db86ef1a990cebe3d5b
Richard Case <richard.case@suse.com>
2023-07-20 16:02:18 +0100

    wip: investigating creating the extension
    Signed-off-by: Richard Case <richard.case@suse.com>

---
+++ pkg/cluster-api/pages/config.vue
@@ -0,0 +1,11 @@
+<script>
+export default { name: 'ConfigPage' };
+</script>
+
+<template>
+    <div>
+        <h1>This is a config VueJS page. You can render anything you want</h1>
+    </div>
+</template>
+
+<style lang="scss" scoped></style>
---
+++ pkg/cluster-api/pages/index.vue
@@ -0,0 +1,11 @@
+<script>
+export default { name: 'DashboardPage' };
+</script>
+
+<template>
+    <div>
+        <h1>This is the dashboard VueJS page. You can render anything you want</h1>
+    </div>
+</template>
+
+<style lang="scss" scoped></style>
